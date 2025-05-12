ae551b3212398eb01d27af33847333366ebf3a8e
0cculus <bri.rhm@gmail.com>
2025-05-12 18:12:36 -0400

    added functions definitions

--- src/parsing/toml_numerals.h
+++ src/parsing/toml_numerals.h
@@ -1,6 +1,12 @@
 #ifndef TOML_NUMERALS
 # define TOML_NUMERALS
 
+# include <stdbool.h>
+# include <string.h>
+# include <ctype.h>
+# include <stdio.h>
 
+bool _toml_verify_within_hexa_limit(char c);
+bool _toml_verify_hexa_value(char* val);
 
 #endif
