d1d550b4316cdb1ff6906af4756933e622cb483e
0cculus <bri.rhm@gmail.com>
2025-05-20 17:56:56 -0400

    added functions for octal and float numbers

--- src/parsing/toml_numerals.c
+++ src/parsing/toml_numerals.c
@@ -1,5 +1,8 @@
 #include "toml_numerals.h"
 
+/*
+ *  Hexadecimal
+ */
 bool _toml_verify_within_hexa_limit(char c) {
 	bool retVal = false;
 	if (isdigit(c))
@@ -16,9 +19,9 @@ bool _toml_verify_hexa_value(char* val) {
 	if (!val)
 			return (false);
 	const size_t lenVal = strlen(val);
-	if (lenVal < 3 || lenVal > 10)
+	if (lenVal < MIN_ || lenVal > MAX_HEXA)
 		return (false);
-	if (val[0] != '0' || val[1] != 'x')
+	if (strncmp(val, "0x", 2) != 0)
 		return (false);
 	int i = 2;
 	while (val[i]) {
@@ -28,4 +31,44 @@ bool _toml_verify_hexa_value(char* val) {
 	}
 	return (true);
 }
-	
+
+/*
+ *  Octal
+ */
+bool _toml_verify_within_octal_limit(char c) {
+	return (c >= '0' && c <= '7');
+}
+
+bool _toml_verify_octal_value(char* val) {
+	if (!val)
+		return (false);
+	const size_t lenVal = strlen(val);
+	if (lenVal < MIN_ || lenVal > 11)
+		return (false);
+	if (strncmp(val, "0o", 2) != 0)
+		return (false);
+	int i = 2;
+	while (val[i]) {
+		if (!_toml_verify_within_octal_limit(val[i]))
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
+/*
+ *  Float, Exponents & Fractionals
+ */
+bool _toml_verify_float_value(char* val, char* allowedSymbols) {
+	if (!val)
+		return (false);
+	int i = 0;
+	while (val[i]) {
+		if (!isdigit(c) && !strchr(allowedSymbols, c))
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
+
--- src/parsing/toml_numerals.h
+++ src/parsing/toml_numerals.h
@@ -6,7 +6,18 @@
 # include <ctype.h>
 # include <stdio.h>
 
+# define MIN_ 3
+# define MAX_HEXA 10
+
+// Hexa
 bool _toml_verify_within_hexa_limit(char c);
 bool _toml_verify_hexa_value(char* val);
+
+// Octal
+bool _toml_verify_within_octal_limit(char c);
+bool _toml_verify_octal_value(char* val);
+
+// Float
+bool _toml_verify_float_value(char* val, char* allowedSymbols);
 
 #endif
